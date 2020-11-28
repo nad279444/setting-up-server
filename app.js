5e6284b04b2aa491b8a84a1ffb8cc44259632e8f
bernard frimpong <nardfrimpong@gmail.com>
2020-11-28 13:29:25 +0000

    routing html pages

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const port  = 3000
 const fs = require('fs')
 
 
+
+
 const server = http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'})
-    fs.readFile('server.html',function(err,data){
-        if(err){
-            res.writeHead(404)
-            res.write('Error: File not found')
-        } else{
-            res.write(data)
-        }
-        res.end()
-    })
     
+        let path = './Docs/';
+        switch (req.url) {
+            case '/':
+                path += 'server.html';
+                break;
+            case '/about':
+                path+= 'about.html';
+                break;
+            default:
+                path+= '404.html';
+                break;
+        }
+        
+        fs.readFile(path,function(err,data){
+            if(err){
+                console.log('got an error',err)
+                res.end()
+            } else{
+                res.write(data)
+                res.end()
+            }
+           
+           
+        })   
+        
 })
 
 server.listen(port,function(err){
