cde2f20e22ca7587b2730990c9f5dadba8ff3dc1
bernard frimpong <nardfrimpong@gmail.com>
2020-11-28 11:56:13 +0000

    read html text from a file system

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http')
 const port  = 3000
+const fs = require('fs')
 
 
 const server = http.createServer(function(req,res){
-    res.write('Hello Node')
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('server.html',function(err,data){
+        if(err){
+            res.writeHead(404)
+            res.write('Error: File not found')
+        } else{
+            res.write(data)
+        }
+        res.end()
+    })
+    
 })
 
 server.listen(port,function(err){
